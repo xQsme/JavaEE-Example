42b8404d9492eadbd4c106dbb4f1783db158f7fa
Pedro Ferreira <pedro-ferreira92@hotmail.com>
2019-08-02 23:41:39 +0100

    good nuff

--- doit/src/main/java/com/airhacks/presentation/Index.java
+++ doit/src/main/java/com/airhacks/presentation/Index.java
@@ -7,6 +7,10 @@ package com.airhacks.presentation;
 
 import com.airhacks.business.reminders.boundary.ToDoManager;
 import com.airhacks.business.reminders.entity.ToDo;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
@@ -25,10 +29,14 @@ import java.util.Set;
 @Model
 public class Index {
 
+    private MenuModel model;
+
     @Inject
     ToDoManager boundary;
 
     ToDo todo;
+
+    ToDo todoToDelete;
 
     @Inject
     Validator validator;
@@ -37,11 +45,27 @@ public class Index {
     public void init()
     {
         this.todo = new ToDo();
+
+        model = new DefaultMenuModel();
+        DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+        DefaultMenuItem item = new DefaultMenuItem("Save");
+        item.setIcon("pi pi-save");
+        item.setUpdate("messages");
+        secondSubmenu.addElement(item);
+        model.addElement(secondSubmenu);
     }
 
     public ToDo getTodo()
     {
         return this.todo;
+    }
+
+    public void setTodo(ToDo todo) {
+        this.todo = todo;
+    }
+
+    public void setTodoToDelete(ToDo todoToDelete) {
+        this.todoToDelete = todoToDelete;
     }
 
     public List<ToDo> getToDos()
@@ -65,13 +89,47 @@ public class Index {
         if(violations.isEmpty())
         {
             this.boundary.save(todo);
-            this.addMessage("Data saved");
+            this.addMessage("Save Attempt", "Data Saved");
         }
         return null;
     }
 
-    public void addMessage(String summary) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Save attempt", summary);
+    public Object select()
+    {
+        this.addMessage("Edit", "Row Selected");
+        System.out.println(todo);
+        return null;
+    }
+
+    public Object cancel()
+    {
+        this.addMessage("Edit", "Edition Canceled");
+        return null;
+    }
+
+    public Object edit()
+    {
+        Set<ConstraintViolation<ToDo>> violations = this.validator.validate(this.todo);
+        for(ConstraintViolation<ToDo> violation : violations)
+        {
+            this.showValidationError(violation.getMessage());
+        }
+        if(violations.isEmpty()) {
+            this.boundary.save(todo);
+            this.addMessage("Edit", "Row Edited");
+        }
+        return null;
+    }
+
+    public Object delete()
+    {
+        this.boundary.delete(todoToDelete.getId());
+        this.addMessage("Delete Attempt", "Row Deleted");
+        return null;
+    }
+
+    public void addMessage(String title, String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, title, summary);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
